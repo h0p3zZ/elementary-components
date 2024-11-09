3334a6b884e1e882fffb8a72d1549febe1e5732d
h0p3zZ <h0p3zZ@outlook.com>
2024-11-09 00:58:15 +0000

    implemented SLBadge

---
+++ Sources/ElementaryShoelace/components/SLBadge.swift
@@ -0,0 +1,35 @@
+import Elementary
+
+public extension HTMLTag {
+    enum SLBadge: HTMLTrait.Shoelace {
+        public static let name = "sl-badge"
+    }
+}
+
+public typealias SLBadge<Content: HTML> = HTMLElement<HTMLTag.SLBadge, Content>
+
+// Should contain all the attributes defined on shoelaces SLBadge component
+// - that can be rendered on the server side and are not only getters
+public extension HTMLAttribute where Tag == HTMLTag.SLBadge {
+    enum SLBadgeVariantValue: String {
+        case primary
+        case success
+        case neutral
+        case warning
+        case danger
+    }
+
+    /// The badge’s theme variant. Default `primary`.
+    /// - Parameter value: The variant of the badge (`primary`, `success`, `neutral`, `warning`, `danger`).
+    /// - Returns: The HTMLAttribute (`variant="(value)"`)
+    static func variant(_ value: SLBadgeVariantValue) -> Self {
+        HTMLAttribute(name: "variant", value: value.rawValue)
+    }
+
+    /// Makes the badge pulsate to draw attention. Default `false`.
+    static var pulse: Self {
+        HTMLAttribute(name: "pulse", value: nil)
+    }
+}
+
+extension HTMLTag.SLBadge: HTMLTrait.Attributes.shoelace.pill {}
