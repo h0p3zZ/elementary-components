3b6c509b5b5c99478580b632768f7724fad9639c
h0p3zZ <h0p3zZ@outlook.com>
2024-11-09 02:15:44 +0000

    finished implementation of SLDetails

--- Sources/ElementaryShoelace/components/SLDetails.swift
+++ Sources/ElementaryShoelace/components/SLDetails.swift
@@ -8,9 +8,16 @@ public extension HTMLTag {
 
 public typealias SLDetails<Content: HTML> = HTMLElement<HTMLTag.SLDetails, Content>
 
-// Component under construction - not all attributes are implemented
+// Should contain all the attributes defined on shoelaces SLDetails component
+// - that can be rendered on the server side and are not only getters
 public extension HTMLAttribute where Tag == HTMLTag.SLDetails {
+    /// The summary to show in the header. If you need to display HTML, use the summary slot instead.
+    /// - Parameter summary: The summary to show in the header.
+    /// - Returns: The HTMLAttribute (`summary="(summary)"`)
     static func summary(_ summary: String) -> Self {
         HTMLAttribute(name: "summary", value: summary)
     }
 }
+
+extension HTMLTag.SLDetails: HTMLTrait.Attributes.shoelace.open {}
+extension HTMLTag.SLDetails: HTMLTrait.Attributes.disabled {}
