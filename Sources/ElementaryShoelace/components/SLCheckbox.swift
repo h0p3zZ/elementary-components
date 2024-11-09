866b99cb17a6db305dc1f0cef9a53bf4c291a2f2
h0p3zZ <h0p3zZ@outlook.com>
2024-11-09 01:40:48 +0000

    implemented SLCheckbox

---
+++ Sources/ElementaryShoelace/components/SLCheckbox.swift
@@ -0,0 +1,39 @@
+import Elementary
+
+public extension HTMLTag {
+    enum SLCheckbox: HTMLTrait.Shoelace {
+        public static let name = "sl-checkbox"
+    }
+}
+
+public typealias SLCheckbox<Content: HTML> = HTMLElement<HTMLTag.SLCheckbox, Content>
+
+// Should contain all the attributes defined on shoelaces SLCheckbox component
+// - that can be rendered on the server side and are not only getters
+public extension HTMLAttribute where Tag == HTMLTag.SLCheckbox {
+    /// Draws the checkbox in a checked state. Default `false`.
+    /// - Returns: The HTMLAttribute (`checked`)
+    static var checked: Self {
+        HTMLAttribute(name: "checked", value: nil)
+    }
+
+    /// Draws the checkbox in an indeterminate state. This is usually applied to checkboxes that represents a “select all/none” behavior when associated checkboxes have a mix of checked and unchecked states. Default `false`.
+    /// - Returns: The HTMLAttribute (`indeterminate`)
+    static var indeterminate: Self {
+        HTMLAttribute(name: "indeterminate", value: nil)
+    }
+
+    /// Draws the checkbox in an indeterminate state. This is usually applied to checkboxes that represents a “select all/none” behavior when associated checkboxes have a mix of checked and unchecked states. Default `false`.
+    /// - Returns: The HTMLAttribute (`indeterminate`)
+    static var defaultChecked: Self {
+        HTMLAttribute(name: "defaultChecked", value: nil)
+    }
+}
+
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.name {}
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.value {}
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.shoelace.size {}
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.disabled {}
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.form {}
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.required {}
+extension HTMLTag.SLCheckbox: HTMLTrait.Attributes.shoelace.helpText {}
