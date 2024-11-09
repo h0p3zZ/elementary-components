d44fe88a6ecd360ed3278f6a5685c09661cf2669
h0p3zZ <h0p3zZ@outlook.com>
2024-11-09 01:31:52 +0000

    finished implementation of SLButton

--- Sources/ElementaryShoelace/components/SLButton.swift
+++ Sources/ElementaryShoelace/components/SLButton.swift
@@ -8,7 +8,8 @@ public extension HTMLTag {
 
 public typealias SLButton<Content: HTML> = HTMLElement<HTMLTag.SLButton, Content>
 
-// Component under construction - not all attributes are implemented
+// Should contain all the attributes defined on shoelaces SlButton component
+// - that can be rendered on the server side and are not only getters
 public extension HTMLAttribute where Tag == HTMLTag.SLButton {
     enum ButtonVariant: String {
         case `default`
@@ -20,35 +21,122 @@ public extension HTMLAttribute where Tag == HTMLTag.SLButton {
         case text
     }
 
+    /// The button’s theme variant. Default `default`.
+    /// - Parameter variant: The button variant (`default`, `primary`, `success`, `neutral`, `warning`, `danger`, `text`).
+    /// - Returns: The HTMLAttribute (`variant="(value)"`)
     static func variant(_ variant: ButtonVariant) -> Self {
         HTMLAttribute(name: "variant", value: variant.rawValue)
     }
 
-    static func download(_ url: String) -> Self {
-        HTMLAttribute(name: "download", value: url)
-    }
-
-    static var outline: Self {
-        HTMLAttribute(name: "outline", value: nil)
-    }
-
-    static var circle: Self {
-        HTMLAttribute(name: "circle", value: nil)
-    }
-
+    /// Draws the button with a caret. Used to indicate that the button triggers a dropdown menu or similar behavior. Default `false`.
+    /// - Returns: The HTMLAttribute (`caret`)
     static var caret: Self {
         HTMLAttribute(name: "caret", value: nil)
     }
 
+    /// Draws the button in a loading state. Default `false`.
+    /// - Returns: The HTMLAttribute (`loading`)
     static var loading: Self {
         HTMLAttribute(name: "loading", value: nil)
     }
+
+    /// Draws an outlined button. Default `false`.
+    /// - Returns: The HTMLAttribute (`outline`)
+    static var outline: Self {
+        HTMLAttribute(name: "outline", value: nil)
+    }
+
+    /// Draws a circular icon button. When this attribute is present, the button expects a single <sl-icon> in the default slot. Default `false`.
+    /// - Returns: The HTMLAttribute (`circle`)
+    static var circle: Self {
+        HTMLAttribute(name: "circle", value: nil)
+    }
+
+    enum SLButtonTypeValue: String {
+        case button
+        case subnmit
+        case reset
+    }
+
+    /// The type of button. Note that the default value is button instead of submit, which is opposite of how native <button> elements behave. When the type is submit, the button will submit the surrounding form. Default `button`.
+    /// - Parameter type: The button type (`button`, `submit`, `reset`).
+    /// - Returns: The HTMLAttribute (`type="(value)"`)
+    static func type(_ type: SLButtonTypeValue) -> Self {
+        HTMLAttribute(name: "type", value: type.rawValue)
+    }
+
+    /// Tells the browser to download the linked file as this filename. Only used when href is present.
+    /// - Parameter url: The URL to download.
+    /// - Returns: The HTMLAttribute (`download="(url)"`)
+    static func download(_ url: String) -> Self {
+        HTMLAttribute(name: "download", value: url)
+    }
+
+    /// Used to override the form owner’s action attribute.
+    /// - Parameter url: The URL to override the form owner’s action attribute.
+    /// - Returns: The HTMLAttribute (`formaction="(url)"`)
+    static func formAction(_ url: String) -> Self {
+        HTMLAttribute(name: "formaction", value: url)
+    }
+
+    enum SLButtonFormEnctypeValue: String {
+        case applicationXWWWFormUrlencoded = "application/x-www-form-urlencoded"
+        case multipartFormData = "multipart/form-data"
+        case textPlain = "text/plain"
+    }
+
+    /// Used to override the form owner’s enctype attribute. Default ` `.
+    /// - Parameter enctype: The form owner’s enctype attribute (`application/x-www-form-urlencoded`, `multipart/form-data`, `text/plain`).
+    /// - Returns: The HTMLAttribute (`formenctype="(enctype)"`)
+    static func formEnctype(_ enctype: SLButtonFormEnctypeValue) -> Self {
+        HTMLAttribute(name: "formenctype", value: enctype.rawValue)
+    }
+
+    enum SLButtonFormMethodValue: String {
+        case get
+        case post
+    }
+
+    /// Used to override the form owner’s method attribute. Default ` `.
+    /// - Parameter method: The selected method (`get`, `post`).
+    /// - Returns: The HTMLAttribute (`formmethod="(method)"`)
+    static func formMethod(_ method: SLButtonFormMethodValue) -> Self {
+        HTMLAttribute(name: "formmethod", value: method.rawValue)
+    }
+
+    /// Used to override the form owner’s novalidate attribute. Default `false`.
+    /// - Returns: The HTMLAttribute (`formnovalidate`)
+    static var formNoValidate: Self {
+        HTMLAttribute(name: "formnovalidate", value: nil)
+    }
+
+    struct SLButtonFormTargetValue: RawRepresentable {
+        public var rawValue: String
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public static var blank: Self { .init(rawValue: "_blank") }
+        public static var _self: Self { .init(rawValue: "_self") }
+        public static var _parent: Self { .init(rawValue: "_parent") }
+        public static var _top: Self { .init(rawValue: "_top") }
+    }
+
+    /// Used to override the form owner’s `target` attribute.
+    /// - Parameter target: The target attribute (`_blank`, `_self`, `_parent`, `_top`).
+    /// - Returns: The HTMLAttribute (`formtarget="(target)"`)
+    static func formTarget(_ target: SLButtonFormTargetValue) -> Self {
+        HTMLAttribute(name: "formtarget", value: target.rawValue)
+    }
 }
 
+extension HTMLTag.SLButton: HTMLTrait.Attributes.shoelace.size {}
+extension HTMLTag.SLButton: HTMLTrait.Attributes.disabled {}
+extension HTMLTag.SLButton: HTMLTrait.Attributes.shoelace.pill {}
+extension HTMLTag.SLButton: HTMLTrait.Attributes.name {}
+extension HTMLTag.SLButton: HTMLTrait.Attributes.value {}
 extension HTMLTag.SLButton: HTMLTrait.Attributes.href {}
 extension HTMLTag.SLButton: HTMLTrait.Attributes.target {}
-extension HTMLTag.SLButton: HTMLTrait.Attributes.disabled {}
-extension HTMLTag.SLButton: HTMLTrait.Attributes.name {}
-
-extension HTMLTag.SLButton: HTMLTrait.Attributes.shoelace.size {}
-extension HTMLTag.SLButton: HTMLTrait.Attributes.shoelace.pill {}
+extension HTMLTag.SLButton: HTMLTrait.Attributes.rel {}
+extension HTMLTag.SLButton: HTMLTrait.Attributes.form {}
